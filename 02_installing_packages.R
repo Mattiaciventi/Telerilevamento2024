5c5e9d56c37af9319c99c2d84624772a0352b71a
Mattiaciventi <149521935+Mattiaciventi@users.noreply.github.com>
2024-03-07 17:50:05 +0100

    Create 02_installing_packages.R

---
+++ 02_installing_packages.R
@@ -0,0 +1,6 @@
+# Installo nuovo pacchetti in R
+
+# tutte le volte che voglio uscire da R x installare pacchetti esterni dal CRAN devo usare ""
+install.packages("terra")
+#per testare se il pacchetto che andrò ad usare gia è caricato uso library
+library(terra)
