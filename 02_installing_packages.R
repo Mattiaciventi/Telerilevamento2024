5ab488b2c1c08193b247731cf058d3dcc31a2f2b
Mattiaciventi <149521935+Mattiaciventi@users.noreply.github.com>
2024-03-12 17:31:44 +0100

    Update 02_installing_packages.R

--- 02_installing_packages.R
+++ 02_installing_packages.R
@@ -4,3 +4,12 @@
 install.packages("terra")
 #per testare se il pacchetto che andrò ad usare gia è caricato uso library
 library(terra)
+
+# per utilizzare la funzione install.github per prendere i pacchetti da github dobbiamo installare in precedenza devtools. quindi install.github dipende da da devtools
+# devtools è dentro il cran
+#una volta installato devtools, attraverso la funzione install.github installo il pacchetto imagery.
+install.packages("devtools")
+library(devtools)
+#installare il pacchetto imageRy da github
+devtools::install_github("ducciorocchini/imageRy") #:: per far capire da dove viene il pacchetto. stiamo uscendo da R e quindi "".
+library(imageRy)
