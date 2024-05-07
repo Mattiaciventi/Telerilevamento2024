bb6563d5be10c34e13b986a43bb773c5e1b1efd6
Mattiaciventi <149521935+Mattiaciventi@users.noreply.github.com>
2024-05-07 16:10:23 +0200

    Update 09_multivariate_analysis.R

--- 09_multivariate_analysis.R
+++ 09_multivariate_analysis.R
@@ -19,4 +19,7 @@ sentdo <- c(b2, b3, b4, b8)
 im.plotRGB(sentdo, r=4, g=3, b=2)
 im.plotRGB(sentdo, r=3, g=4, b=2)
 
+#La funzione pairs() in R è utilizzata per creare una matrice di scatterplot che mostra le relazioni tra 
+#tutte le coppie di variabili in un set di dati. È particolarmente utile quando si desidera esaminare le 
+#correlazioni e le distribuzioni tra più variabili contemporaneamente.
 pairs(sentdo)
